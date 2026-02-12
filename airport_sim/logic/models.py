da92eb3cde6f6b2e114acd7e07b2c8d925a891ca
Federico Parisella <darthmorphus@gmail.com>
2026-02-12 15:39:01 +0000

    Initialised queue + runway logic, added emergency time left to Plane to account for different emergencies having different priorities

--- airport_sim/logic/models.py
+++ airport_sim/logic/models.py
@@ -3,6 +3,10 @@
 from enum import Enum
 from datetime import datetime
 
+
+# hello
+
+
 class EmergencyStatus(Enum):
     NONE = 0
     FUEL = 1
@@ -26,6 +30,7 @@ class Plane:
         self.is_departure = is_departure
         self.fuel_level = fuel_level
         self.emergency_status = emergency_status
+        self.emergency_time_left = 0 # Initially 0, will be decreased in decrease fuel when emergency arises
         self.target_time = None
         self.actual_time = None
         self.current_location = origin
@@ -44,4 +49,8 @@ class Plane:
 
 class Runway:
     def __init__(self, is_departure: bool, mixed_mode: bool, runway_number: int, is_available: bool, is_operational: bool):
-        pass
+        self.is_departure = is_departure
+        self.mixed_mode = mixed_mode
+        self.runway_number = runway_number
+        self.is_available = is_available
+        self.is_operational = is_operational
