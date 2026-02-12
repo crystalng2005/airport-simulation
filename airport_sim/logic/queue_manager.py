c767b18b7ce8c250b01919f213ee31a8d86f73f2
Federico Parisella <darthmorphus@gmail.com>
2026-02-12 15:36:07 +0000

    Reverts latest commit (accidentally pushed to main :/ This reverts commit 28ca56fa55b92f44c74b2a3d41ded30e63c4cc42.

--- airport_sim/logic/queue_manager.py
+++ airport_sim/logic/queue_manager.py
@@ -1,43 +1,23 @@
 # QueueController Class
+from typing import List
+from queue import Queue
 from models import Plane, Runway, EmergencyStatus
 from datetime import datetime
 
 class QueueController:
-    def __init__(self, plane_queue: list[Plane], runway_list: list[Runway], is_departure: bool):
-        self.plane_queue = plane_queue # A list, treated as a queue
+    def __init__(self, plane_queue: Queue[Plane], runway_list: List[Runway], is_departure: bool):
+        self.plane_queue = plane_queue
         self.runway_list = runway_list
-        self.is_departure = is_departure # true = takeoff, false = landing 
+        self.is_departure = is_departure
 
-    def checkRunways(self): 
-        for runway in self.runway_list:
-            if runway.is_available and len(self.plane_queue) != 0:
-                self.plane_queue.pop(0).goToRunway(runway.runway_number)
+    def checkRunways(self) -> bool: # I dont know what this returns
+        pass
 
-    def enqueue(self, p: Plane):
-        self.plane_queue.append(Plane)
+    def enqueue(self, p: Plane) -> bool:
+        pass
 
     def checkCancelTime(self) -> datetime:
-        if self.is_departure:
-            for plane in self.plane_queue:
-                if plane.emergency_status == EmergencyStatus.NONE:
-                    time = plane.target_time
-                    # Call simulationController -> get the specified cancellation time threshold
-                    # if over the threshold, cancel plane
-        
+        pass
 
     def planeEmergency(self, p: Plane) -> EmergencyStatus:
-        match (p.emergency_status):
-            case EmergencyStatus.FUEL:
-                p.emergency_time_left = 10
-            case EmergencyStatus.HEALTH:
-                p.emergency_time_left = 20
-            case EmergencyStatus.MECHANICAL:
-                p.emergency_time_left = 30
-        temp = 0
-        for plane in self.plane_queue:
-                if plane.emergency_time_left < p.emergency_time_left:
-                    temp+= 1
-                else:
-                    self.plane_queue.insert(temp, p)
-                    break
-
+        pass
