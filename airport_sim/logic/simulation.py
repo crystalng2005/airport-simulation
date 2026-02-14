d0d078499997863edbff88cc1a3ca07827e54bbf
Saroop <ssjagdev@outlook.com>
2026-02-14 15:50:17 +0000

    generating planes and runways

--- airport_sim/logic/simulation.py
+++ airport_sim/logic/simulation.py
@@ -1,4 +1,10 @@
 # Simulation Controller
+from datetime import datetime, timedelta
+from plane import Plane
+from models import Runway
+from queue_manager import QueueController
+from report import PerformanceReport
+import random
 
 class SimulationController:
     def __init__(
@@ -23,13 +29,32 @@ class SimulationController:
         pass
 
     def generatePlane(self, is_departure: bool) -> bool:
-        pass
+        p = Plane(is_departure, self.time)
+        if is_departure:
+            self.departure_queue.enqueue(p)
+        else:
+            self.landing_queue.enqueue(p)
+
+        self.report.total_planes += 1
 
     def generateQueue(self) -> bool:
         pass
 
     def generateRunway(self) -> bool:
-        pass
+        self.runway_list = []
+        runway_num = 1
+        for i in range(self.departure_runways):
+            self.runway_list.append(Runway(True, False, runway_num, True, True))
+            runway_num += 1
+
+        for i in range(self.landing_runways):
+            self.runway_list.append(Runway(False, False, runway_num, True, True))
+            runway_num += 1
+        
+        for i in range(self.mixed_runways):
+            self.runway_list.append(Runway(True, True, runway_num, True, True))
+            runway_num += 1
+        return True
 
     def update(self) -> bool:
         pass
