2589e92c3280e35797694f29850fabfb5dac2ccd
Saroop <ssjagdev@outlook.com>
2026-02-19 23:04:24 +0000

    report.py complete

--- airport_sim/logic/report.py
+++ airport_sim/logic/report.py
@@ -8,8 +8,21 @@ class PerformanceReport:
         self.tot_wait_time = tot_wait_time
         self.tot_fuel_used = tot_fuel_used
 
-    def output(self) -> str:
-        pass
+    def output(self) -> dict:
+        avg_wait = (
+            self.tot_wait_time / self.total_planes
+            if self.total_planes > 0
+            else 0
+        )
+
+        return {
+            "total_planes": self.total_planes,
+            "cancellations": self.cancellations,
+            "diversions": self.diversions,
+            "average_wait_time": round(avg_wait, 2),
+            "total_fuel_used": round(self.tot_fuel_used, 2),
+        }
 
     def reset(self) -> bool:
-        pass
+        self.__init__()
+        return True
