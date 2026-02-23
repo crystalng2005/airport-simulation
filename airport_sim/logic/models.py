ea1d1393c4536b9fa2b85f5ac9800a086f0aa18b
Turki-84 <turki.dossary2014@gmail.com>
2026-02-23 06:18:02 +0000

    .

--- airport_sim/logic/models.py
+++ airport_sim/logic/models.py
@@ -3,45 +3,10 @@
 from enum import Enum
 from datetime import datetime
 
-class EmergencyStatus(Enum):
-    NONE = 0
-    FUEL = 1
-    HEALTH = 2
-    MECHANICAL = 3
-
-
-class Plane:
-    def __init__(
-        self,
-        callsign: str,
-        origin: str,
-        destination: str,
-        is_departure: bool,
-        fuel_level: float,
-        emergency_status: EmergencyStatus = EmergencyStatus.NONE,
-    ):
-        self.callsign = callsign
-        self.origin = origin
-        self.destination = destination
-        self.is_departure = is_departure
-        self.fuel_level = fuel_level
-        self.emergency_status = emergency_status
-        self.target_time = None
-        self.actual_time = None
-        self.current_location = origin
-
-    def decreaseFuel(self) -> bool:
-        pass
-
-    def goToRunway(self, runway: int) -> bool:
-        pass
-
-    def cancel(self) -> bool:
-        pass
-
-    def hasEmergency(self) -> bool:
-        pass
-
 class Runway:
     def __init__(self, is_departure: bool, mixed_mode: bool, runway_number: int, is_available: bool, is_operational: bool):
-        pass
+        self.is_departure = is_departure
+        self.mixed_mode = mixed_mode
+        self.runway_number = runway_number
+        self.is_available = is_available
+        self.is_operational = is_operational
