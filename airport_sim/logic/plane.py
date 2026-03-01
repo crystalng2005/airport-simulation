91e29d4e036a6d6b4e268f1c446806705ba1654a
julchr <chrom.julia@outlook.com>
2026-03-01 16:24:40 +0000

    updated conversion ranges for times

--- airport_sim/logic/plane.py
+++ airport_sim/logic/plane.py
@@ -160,9 +160,9 @@ class Plane:
         #doing it in seconds, but can change it to be in minutes as well
         #86400
         randSeconds = random.randint(0,82800)
-        randMinutes = randSeconds/60
+        randMinutes = (randSeconds/60) % 60
         self.tickTargetTime = math.ceil(randMinutes/5)
-        randHours = randMinutes/60
+        randHours = (randMinutes/60) % 60
         return datetime.time(int(randHours), int(randMinutes), int(randSeconds))
 
     def genActualTime(self):
@@ -170,8 +170,8 @@ class Plane:
         time = random.normal(actualSeconds, 5*60) 
         self.tickActualTime = random.normal(self.tickTargetTime, 1)
         timeSeconds = time
-        timeMinutes = timeSeconds/60
-        timeHours = timeMinutes/60
+        timeMinutes = (timeSeconds/60) % 60
+        timeHours = (timeMinutes/60) % 60
         return datetime.time(int(timeHours),int(timeMinutes), int(timeSeconds))
     
   
