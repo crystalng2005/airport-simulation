80d555f289dd7afbc1c269c8a790b15f925ebf17
slappinwack <crystalngshulu9909@gmail.com>
2026-03-10 23:49:01 +0000

    Update app.py

--- airport_sim/app.py
+++ airport_sim/app.py
@@ -410,8 +410,6 @@ def get_saved_report_plots(sim_id):
         return jsonify({'success': False, 'error': str(e)}), 500
 
 
-    
-   
 if __name__ == '__main__':
     port = int(os.environ.get('FLASK_RUN_PORT', os.environ.get('PORT', 8080)))
     app.run(host='127.0.0.1', port=port, debug=True)
