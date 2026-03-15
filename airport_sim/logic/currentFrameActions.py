5f488d25d2415402dd28b5276343f9f399b29498
julchr <chrom.julia@outlook.com>
2026-03-15 15:06:15 +0000

    turned comments into docstrings

--- airport_sim/logic/currentFrameActions.py
+++ airport_sim/logic/currentFrameActions.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass, field
 
 
-# Class to store which frame actions have taken place last tick
-# Stores a list in the form [[plane, "action"]]
+
 @dataclass
 class CurrentFrameActions:
+    """
+    Class to store which frame actions have taken place last tick
+    Stores a list in the form [[plane, "action"]] 
+    """
     current_frame_actions: list[list[str]] = field(default_factory=list)
 
 currentFrameActions = CurrentFrameActions()
